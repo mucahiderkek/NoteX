a2c31d3cfcf0b110f0b7168c5ce17b883d2d9d26
MucahitErkek <94393687+mucahiderkek@users.noreply.github.com>
2025-02-28 16:37:09 +0300

    NoteX

--- NoteX/Form1.Designer.cs
+++ NoteX/Form1.Designer.cs
@@ -28,17 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnYeni = new System.Windows.Forms.Button();
             this.btnAc = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnKaydet = new System.Windows.Forms.Button();
             this.btnCikis = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPdfKaydet = new System.Windows.Forms.Button();
+            this.lblKarakterSayisi = new System.Windows.Forms.Label();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnRenk = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnYeni
             // 
+            this.btnYeni.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnYeni.Location = new System.Drawing.Point(285, 3);
             this.btnYeni.Name = "btnYeni";
             this.btnYeni.Size = new System.Drawing.Size(94, 36);
@@ -49,6 +58,7 @@
             // 
             // btnAc
             // 
+            this.btnAc.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnAc.Location = new System.Drawing.Point(3, 3);
             this.btnAc.Name = "btnAc";
             this.btnAc.Size = new System.Drawing.Size(93, 36);
@@ -59,15 +69,19 @@
             // 
             // textBox1
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.textBox1.Location = new System.Drawing.Point(0, 45);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 44);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(800, 405);
+            this.textBox1.Size = new System.Drawing.Size(1830, 836);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnKaydet
             // 
+            this.btnKaydet.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnKaydet.Location = new System.Drawing.Point(195, 3);
             this.btnKaydet.Name = "btnKaydet";
             this.btnKaydet.Size = new System.Drawing.Size(84, 36);
@@ -78,6 +92,7 @@
             // 
             // btnCikis
             // 
+            this.btnCikis.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnCikis.Location = new System.Drawing.Point(99, 3);
             this.btnCikis.Name = "btnCikis";
             this.btnCikis.Size = new System.Drawing.Size(90, 36);
@@ -88,26 +103,80 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.btnRenk);
+            this.panel1.Controls.Add(this.btnPdfKaydet);
+            this.panel1.Controls.Add(this.lblKarakterSayisi);
+            this.panel1.Controls.Add(this.btnFont);
             this.panel1.Controls.Add(this.btnAc);
             this.panel1.Controls.Add(this.btnCikis);
             this.panel1.Controls.Add(this.btnYeni);
             this.panel1.Controls.Add(this.btnKaydet);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 40);
+            this.panel1.Size = new System.Drawing.Size(937, 40);
             this.panel1.TabIndex = 5;
+
+            // 
+            // btnPdfKaydet
+            // 
+            this.btnPdfKaydet.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnPdfKaydet.BackColor = System.Drawing.Color.Cyan;
+            this.btnPdfKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPdfKaydet.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnPdfKaydet.Location = new System.Drawing.Point(585, 3);
+            this.btnPdfKaydet.Name = "btnPdfKaydet";
+            this.btnPdfKaydet.Size = new System.Drawing.Size(105, 36);
+            this.btnPdfKaydet.TabIndex = 7;
+            this.btnPdfKaydet.Text = "PDF KAydet";
+            this.btnPdfKaydet.UseVisualStyleBackColor = false;
+            this.btnPdfKaydet.Click += new System.EventHandler(this.btnPdfKaydet_Click);
+            // 
+            // lblKarakterSayisi
+            // 
+            this.lblKarakterSayisi.AutoSize = true;
+            this.lblKarakterSayisi.Location = new System.Drawing.Point(796, 9);
+            this.lblKarakterSayisi.Name = "lblKarakterSayisi";
+            this.lblKarakterSayisi.Size = new System.Drawing.Size(0, 16);
+            this.lblKarakterSayisi.TabIndex = 5;
+            // 
+            // btnFont
+            // 
+            this.btnFont.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnFont.Location = new System.Drawing.Point(385, 4);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(94, 36);
+            this.btnFont.TabIndex = 4;
+            this.btnFont.Text = "Yazı Tipi";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // btnRenk
+            // 
+            this.btnRenk.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnRenk.Location = new System.Drawing.Point(485, 3);
+            this.btnRenk.Name = "btnRenk";
+            this.btnRenk.Size = new System.Drawing.Size(94, 36);
+            this.btnRenk.TabIndex = 5;
+            this.btnRenk.Text = "Renk";
+            this.btnRenk.UseVisualStyleBackColor = true;
+            this.btnRenk.Click += new System.EventHandler(this.btnRenk_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 6000;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(937, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NoteX";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +190,13 @@
         private System.Windows.Forms.Button btnKaydet;
         private System.Windows.Forms.Button btnCikis;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnRenk;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lblKarakterSayisi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnPdfKaydet;
     }
 }
 
--- NoteX/Form1.cs
+++ NoteX/Form1.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Speech.Synthesis;
+using System.Drawing;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using NHunspell; // Hunspell kütüphanesini ekledik.
 
 namespace NotDefteri
 {
@@ -10,15 +14,74 @@ namespace NotDefteri
         public Form1()
         {
             InitializeComponent();
+            ApplyButtonStyles();
+            this.Resize += new EventHandler(Form1_Resize);
+            this.MouseWheel += new MouseEventHandler(Form1_MouseWheel); // Formun MouseWheel olayını bağla
         }
 
-        // ✅ Yeni Dosya Butonu
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            panel1.Left = 0;
+            panel1.Top = 0;
+            // Label'ı formun sol alt köşesine sabitle
+            lblKarakterSayisi.Left = 10; // Sol kenardan 10 piksel
+            lblKarakterSayisi.Top = this.ClientSize.Height - lblKarakterSayisi.Height - 10; // Alt kenardan 10 piksel
+        }
+
+        private void ApplyButtonStyles()
+        {
+            foreach (Control control in panel1.Controls)
+            {
+                if (control is Button btn)
+                {
+                    btn.BackColor = Color.FromArgb(224, 242, 255);
+                    btn.ForeColor = Color.FromArgb(64, 64, 64);
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+
+                    btn.MouseEnter += (sender, e) => btn.BackColor = Color.FromArgb(192, 229, 255);
+                    btn.MouseLeave += (sender, e) => btn.BackColor = Color.FromArgb(224, 242, 255);
+                }
+            }
+            panel1.BackColor = Color.FromArgb(240, 248, 255);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Font = new System.Drawing.Font("Segoe UI", 10);
+            textBox1.Font = new System.Drawing.Font("Arial", 12);
+            ApplyButtonStyles();
+            textBox1.MouseWheel += new MouseEventHandler(textBox1_MouseWheel);
+            // Label'ı formun sol alt köşesine sabitle
+            lblKarakterSayisi.Left = 10; // Sol kenardan 10 piksel
+            lblKarakterSayisi.Top = this.ClientSize.Height - lblKarakterSayisi.Height - 10; // Alt kenardan 10 piksel
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // Ctrl tuşu basılıysa yazı tipi boyutunu büyüt/küçült
+            if (Control.ModifierKeys == Keys.Control)
+            {
+                float currentSize = textBox1.Font.Size;
+                float newSize = currentSize + (e.Delta > 0 ? 1 : -1); // Yukarı ise büyüt, aşağı ise küçült
+
+                if (newSize >= 8 && newSize <= 72) // Boyut sınırları
+                {
+                    textBox1.Font = new System.Drawing.Font(textBox1.Font.FontFamily, newSize, textBox1.Font.Style);
+                }
+            }
+        }
+
+        private void textBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // Bu metodu boş bırakabilirsiniz, çünkü Form1_MouseWheel metodu kullanılacak.
+        }
+
         private void btnYeni_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
         }
 
-        // ✅ Dosya Açma Butonu
         private void btnAc_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFile = new OpenFileDialog();
@@ -29,7 +92,6 @@ namespace NotDefteri
             }
         }
 
-        // ✅ Dosya Kaydetme Butonu
         private void btnKaydet_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFile = new SaveFileDialog();
@@ -40,10 +102,88 @@ namespace NotDefteri
             }
         }
 
-        // ✅ Çıkış Butonu
         private void btnCikis_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Font = fontDialog.Font;
+            }
+        }
+
+        private void btnRenk_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lblKarakterSayisi.Text = "Karakter Sayısı: " + textBox1.Text.Length.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string filePath = "otomatik_Kayir.txt";
+            File.WriteAllText(filePath, textBox1.Text);
+        }
+
+        private void btnPdfKaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "PDF Dosyaları|*.pdf";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                Document doc = new Document();
+                PdfWriter.GetInstance(doc, new FileStream(saveFile.FileName, FileMode.Create));
+                doc.Open();
+                doc.Add(new Paragraph(textBox1.Text));
+                doc.Close();
+            }
+        }
+
+        // ✅ Hunspell ile Yazım Denetimi Butonu
+        private void btnSpellCheck_Click(object sender, EventArgs e)
+        {
+            string dicPath = Path.Combine(Application.StartupPath, "tr_TR.dic");
+            string affPath = Path.Combine(Application.StartupPath, "tr_TR.aff");
+
+            if (!File.Exists(dicPath) || !File.Exists(affPath))
+            {
+                MessageBox.Show("Dil dosyaları bulunamadı! Lütfen 'tr_TR.dic' ve 'tr_TR.aff' dosyalarını proje klasörüne ekleyin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (Hunspell hunspell = new Hunspell(affPath, dicPath))
+            {
+                string[] words = textBox1.Text.Split(' ');
+                string incorrectWords = "";
+
+                foreach (string word in words)
+                {
+                    if (!hunspell.Spell(word))
+                    {
+                        incorrectWords += word + "\n";
+                    }
+                }
+
+                if (incorrectWords != "")
+                {
+                    MessageBox.Show("Yanlış Kelimeler:\n" + incorrectWords, "Yazım Denetimi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Tüm kelimeler doğru!", "Yazım Denetimi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
         }
     }
 }
